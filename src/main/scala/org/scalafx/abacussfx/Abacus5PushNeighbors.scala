cf00bca4a9fea81afdb3a4ff01fb5c3b6f99a4b4
Jarek Sacha <jpsacha@gmail.com>
2013-11-02 22:22:26 -0400

    Correct code to work similar to JavaFX original.
    Clicking on balls will properly toggle their location to left or right. Size of the main window was also corrected. Couple of other smaller improvements.
    The overall flow of creation of the balls/circles can be futher optimized.

--- src/main/scala/org/scalafx/abacussfx/Abacus5PushNeighbors.scala
+++ src/main/scala/org/scalafx/abacussfx/Abacus5PushNeighbors.scala
@@ -1,4 +1,5 @@
 package org.scalafx.abacussfx
+
 /*
  * Copyright (c) 2013, ScalaFX Project
  * All rights reserved.
@@ -25,101 +26,90 @@ package org.scalafx.abacussfx
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.shape.{ Circle, Rectangle }
+import scalafx.scene.shape.{Circle, Rectangle}
 import scalafx.scene.input.MouseEvent
 import scalafx.animation.TranslateTransition
 import scalafx.scene.paint.Color._
 import scalafx.scene.text.Text
-import scalafx.scene.layout.Pane
 import scalafx.util.Duration
-import javafx.beans.value.ChangeListener
-import javafx.beans.value.ObservableValue
 
 /**
-  * This program shows how to draw rails using Rectangle shape and using text.
-  *
-  *  @author Rajmahendra Hegde <rajmahendra@gmail.com>
-  */
+ * This program shows how to draw rails using Rectangle shape and using text.
+ *
+ * @author Rajmahendra Hegde <rajmahendra@gmail.com>
+ */
 object Abacus5PushNeighbors extends JFXApp with AbacusCommons {
 
-    var circles: Seq[Circle] = null
-    var rails: Seq[Rectangle] = null
-    var texts: Seq[Text] = Seq empty
+  val rails = makeRails()
+  var texts: Seq[Text] = Seq.empty
 
-    rails = makeRails
-
-    circles = for (
-        row <- 0 to ROW_COUNT - 1;
-        col <- 0 to COL_COUNT - 1
-    ) yield {
-        var last: Circle = null
-        val ball = new Circle {
-            radius = RADIUS - 1
-            centerX = OFFSET + (col * DIAMETER)
-            centerY = OFFSET + (row * DIAMETER)
+  val circles = for (row <- 0 to ROW_COUNT - 1) yield {
+    var previousBall: Circle = null
+    for (col <- 0 to COL_COUNT - 1) yield {
+      val currentBall = new Circle {
+        thisBall =>
+        radius = RADIUS - 1
+        centerX = OFFSET + (col * DIAMETER)
+        centerY = OFFSET + (row * DIAMETER)
+        onMouseClicked = (e: MouseEvent) => {
+          // Toggle between `0` and `MOVE_WAY` locations.
+          new TranslateTransition {
+            node = thisBall
+            toX = if (translateX() > 1) 0 else MOVE_WAY
+            duration = Duration(200)
+          }.playFromStart()
         }
-        ball.onMouseClicked = (e: MouseEvent) => {
-            val translateBall = new TranslateTransition {
-                node = ball
-                toX = if (ball.translateX() > 1) 0 else MOVE_WAY
-                duration = Duration(200)
-            }.playFromStart
-        }
+      }
 
-        texts = texts :+ makeText(((COL_COUNT - col) % COL_COUNT) + "", ball)
+      texts = texts :+ makeText(((COL_COUNT - col) % COL_COUNT) + "", currentBall)
 
-        if (last != null) {
-            last.translateXProperty.addListener(new ChangeListener[Any] {
-                override def changed(observable: ObservableValue[_], oldValue: Any, newValue: Any) {
-                    val newX = newValue.asInstanceOf[Number]
-                    if (newX.doubleValue > ball.translateXProperty().get)
-                        ball setTranslateX newX.doubleValue
-                }
-            })
-            val finalLast = last
-            ball.translateXProperty().addListener(new ChangeListener[Any] {
-                override def changed(observable: ObservableValue[_], oldValue: Any, newValue: Any) {
-                    val newX = newValue.asInstanceOf[Number]
-                    if (newX.doubleValue > finalLast.translateXProperty().get)
-                        ball setTranslateX newX.doubleValue
-                }
-            })
+      if (previousBall != null) {
+        // We need a `value` reference to the current currentBall, so it is properly referenced to during
+        // later invocation in `onChange` event handler.
+        val leftBall = previousBall
 
-        }
-        last = ball
-        ball
+        // If the ball on the left moved to the right, make sure that current ball toggles to the right too
+        leftBall.translateX.onChange((_, _, newValue) => {
+          val newX = newValue.doubleValue
+          if (newX > currentBall.translateX()) currentBall.translateX = newX
+        })
+        // If current current ball toggled to the left, toggle ball on the left to the left too
+        currentBall.translateX.onChange((_, _, newValue) => {
+          val newX = newValue.doubleValue
+          if (newX < leftBall.translateX()) leftBall.translateX = newX
+        })
+
+      }
+      previousBall = currentBall
+      currentBall
     }
+  }
 
-    private def makeRails: Seq[Rectangle] = for (row <- 0 to ROW_COUNT - 1)
-        yield new Rectangle {
-        width = WIDTH
-        height = RAIL_HEIGHT
-        x = PADDING
-        y = OFFSET - (RAIL_HEIGHT / 2) + (row * DIAMETER)
+  private def makeRails(): Seq[Rectangle] = for (row <- 0 to ROW_COUNT - 1) yield new Rectangle {
+    width = WIDTH
+    height = RAIL_HEIGHT
+    x = PADDING
+    y = OFFSET - (RAIL_HEIGHT / 2) + (row * DIAMETER)
+  }
+
+  private def makeText(label: String, ball: Circle) = new Text {
+    x = ball.centerX() - 3
+    y = ball.centerY() + 4
+    text = label //"" + ((COL_COUNT - col) % COL_COUNT)
+    translateX <== ball.translateX
+    onMouseClicked = ball.onMouseClicked()
+    fill = WHITE
+  }
+
+  stage = new PrimaryStage {
+    title = "Abacus 5 Push Neighbors"
+    scene = new Scene(WIDTH + 2 * PADDING, HEIGHT + 2 * PADDING) {
+      content = rails ++ circles.flatten ++ texts
     }
-
-    private def makeText(label: String, ball: Circle): Text = {
-        new Text {
-            x = ball.getCenterX - 3
-            y = ball.getCenterY + 4
-            text = label //"" + ((COL_COUNT - col) % COL_COUNT)
-            translateX bind ball.translateX
-            onMouseClicked = ball.onMouseClicked
-            fill = WHITE
-        }
-    }
-
-    stage = new PrimaryStage {
-        title = "Abacus 5 Push Neighbors"
-        width = WIDTH + 2 * PADDING
-        height = HEIGHT + 2 * PADDING
-
-        scene = new Scene {
-            content = rails ++ circles ++ texts
-        }
-    }
+  }
 }
